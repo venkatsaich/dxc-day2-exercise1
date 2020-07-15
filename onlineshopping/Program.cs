994678786efeeb50f80f60aef53020c60f67827e
venkatsaich <venkatsaichodipalligmail.com>
2020-07-15 07:52:42 +0530

    first commit

---
+++ onlineshopping/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlineshopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            customer c1 = new customer();
+            c1.register("venkat","1234","venkat@gmail.com","venkat@gmail.com","12332", "male");
+            c1.display();
+        }
+    }
+}
---
+++ onlineshopping/customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlineshopping
+{
+    class customer
+    {
+        internal string name;
+        internal string phno;
+        internal string emailid;
+       internal  string loginid;
+        internal string password;
+        internal string gender;
+
+
+        internal void register(string name, string phno,string emailid,
+            string loginid,string password,string gender)
+            {
+            this.name = name;
+            this.phno = phno;
+            this.emailid = emailid;
+            this.loginid = loginid;
+            this.password = password;
+            this.gender = gender;
+            }
+        internal void display()
+        {
+            Console.WriteLine($"Name: {name} phnumber: {phno} " + $"password : {password} gender : {gender}");
+
+        }
+    }
+
+}
